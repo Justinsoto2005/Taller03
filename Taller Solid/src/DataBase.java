e94d6f7592d8cd210cb2adc3a301d1574158dedc
DannaUrdanigo <dannurda@espol.edu.ec>
2025-10-15 16:18:34 -0500

    Aplicando principio dependency Inversion

--- Taller Solid/src/DataBase.java
+++ Taller Solid/src/DataBase.java
@@ -1,3 +1,16 @@
-public class DataBase {
+public interface DataBase {
+    public void insert(String statement);
+    public void select(String statement);
+    public void delete(String statement);
+    public void update(String statement);
 
-}
+
+    //Se agrego esta interfaz por el principio de Dependency Inversion, ya que en el AppWeb
+    //dependia concretamente de MySql, pero si en el futuro se desea agregar bases de datos relacionales
+    //como PostgreSQL o SQL Server, no se podria ya que AppWeb depende de la clase MySQL en concreto,
+    //pero ahora con esta interfaz DataBase ya se podrian crear varias clases en la cual en cada una haya
+    //diferente bases de datos y en AppWeb se instanciaria la interfaz DataBase cosa que ahi ya dependeria
+    //de esta y asi se deja de violar el principio de Dependecy Inversiond
+} 
+
+
